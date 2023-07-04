d5f4312f3441ec9559dea5b27b9f01989b7cc004
lukagoderdzishvili <l_goderdzishvili2@cu.edu.ge>
2023-07-04 23:45:36 +0400

    List Foods

---
+++ frontend/src/app/services/food.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Food } from '../shared/models/Food';
+import { sample_foods } from 'src/data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FoodService {
+
+  constructor() { }
+
+  public getAll(): Array<Food>{
+    return sample_foods;
+  }
+}
