a1af61a4734f5aedc09b2e5b135aacfa356f65d7
lukagoderdzishvili <l_goderdzishvili2@cu.edu.ge>
2023-07-09 18:23:40 +0400

    MakeComponentsForLoginPage

---
+++ frontend/src/app/components/partials/input-validation/input-validation.component.ts
@@ -0,0 +1,45 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AbstractControl, ValidationErrors } from '@angular/forms';
+
+const VALIDATORS_MESSAGES:any = {
+  required: 'Should not be empty',
+  email: 'Email is not valid'
+}
+
+@Component({
+  selector: 'input-validation',
+  templateUrl: './input-validation.component.html',
+  styleUrls: ['./input-validation.component.scss']
+})
+export class InputValidationComponent implements OnInit, OnChanges {
+  @Input()
+  control!: AbstractControl;
+  @Input()
+  showErrorsWhen: boolean = true;
+  public errorMessages: Array<string> = [];
+
+  ngOnInit(): void {
+    this.control.statusChanges.subscribe(() => {
+      this.checkValidation();
+    });
+
+    this.control.valueChanges.subscribe(() => {
+      this.checkValidation();
+    });
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    this.checkValidation();
+  }
+
+  public checkValidation(): void {
+    const errors: ValidationErrors | null = this.control.errors;
+    if(!errors){
+      this.errorMessages = [];
+      return
+    }
+
+    const errorKeys = Object.keys(errors);
+    this.errorMessages = errorKeys.map(key => VALIDATORS_MESSAGES[key]);
+  }
+}
